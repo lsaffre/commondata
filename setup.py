1f611f82b98eb15dab0fe0b3d7ae4ab9179a1d31
Luc Saffre <luc.saffre@gmail.com>
2016-08-21 05:02:25 +0200

    http://luc.lino-framework.org/blog/2016/0821.html

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 SETUP_INFO = dict(
     name='commondata',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=[],
     description="A structured collection of common knowledge",
     license='GPL',
