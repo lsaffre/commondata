9fe64fc09b6c0154ca0c5fbab18c41f5bd4ae72c
Luc Saffre <luc.saffre@gmail.com>
2021-04-09 13:19:55 +0300

    update tasks.py syntax

--- tasks.py
+++ tasks.py
@@ -1,6 +1,4 @@
 from atelier.invlib import setup_from_tasks
-ns = setup_from_tasks(globals())
-ns.configure({
-    'revision_control_system': 'git',
-    'doc_trees': [],
-})
+ns = setup_from_tasks(globals(),
+    revision_control_system='git',
+    doc_trees=[])
